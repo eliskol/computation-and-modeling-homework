b0cd77a72463e879d2e95db8ca5513e5b0d828d5
Elias <75465281+eliskol@users.noreply.github.com>
2021-08-31 01:33:40 +0000

    Implemented is_prime, get_intersection, get_union, count_characters, get_first_n_terms_nonrecursive, get_nth_term_recursive, convert_to_base_10, convert_to_base_2 functions, and test cases for every function

--- 001/code.py
+++ 001/code.py
@@ -1,3 +1,6 @@
+import math
+
+
 def sum_of_first_n_numbers(n):
     sum = 0
     for i in range(1, n+1):
@@ -8,7 +11,7 @@ def sum_of_first_n_numbers(n):
 def check_for_symmetry(input_string):
     if input_string == "":
         return True
-    return input_string == input_string[::-1]
+    return input_string == input_string[:: - 1]
 
 
 def convert_to_numbers(input_string):
@@ -42,5 +45,113 @@ def is_prime(n):
         if n % i == 0:
             return False
 
-        else:
+        elif i == (n//2):
             return True
+
+
+def get_intersection(list1, list2):
+
+    output_list = []
+
+    for item in list1:
+
+        if item in list2:
+
+            if item not in output_list:
+
+                output_list.append(item)
+
+    return output_list
+
+
+def get_union(list1, list2):
+    output_list = []
+
+    for item in list1:
+        if item not in output_list:
+           output_list.append(item)
+
+    for item in list2:
+        if item not in list1:
+            output_list.append(item)
+
+    return output_list
+
+
+def count_characters(input_string):
+
+    output_dict = {}
+
+    lowercase_input_string = input_string.lower()
+
+    for character in lowercase_input_string:
+        output_dict[character] = lowercase_input_string.count(character)
+
+    return output_dict
+
+
+def get_first_n_terms_nonrecursive(n):
+
+    term_list = []
+
+    for i in range(1, n+1):
+
+        if i == 1:
+            new_term = 5
+            term_list.append(new_term)
+
+        else:
+            new_term = 3 * (term_list[i-2]) - 4
+            term_list.append(new_term)
+
+    return term_list
+
+
+def get_nth_term_recursive(n):
+
+    if n == 1:
+        return 5
+
+    else:
+        return 3 * get_nth_term_recursive(n-1) - 4
+
+
+def convert_to_base_10(input_binary):
+
+    input_binary_string = str(input_binary)
+
+    reversed_binary_string = input_binary_string[::-1]
+
+    output = 0
+
+    for index, number in enumerate(reversed_binary_string):
+
+        if int(number) == 1 or int(number) == 0:
+
+            output += int(number) * 2**index
+
+        else:
+            return("input is not binary!")
+
+    return(output)
+
+
+def convert_to_base_2(n):
+
+    exponent_list = []
+
+    output = 0
+
+    while n > 0:
+
+        n_log_base_2 = math.log(n, 2)
+        rounded_log = math.floor(n_log_base_2)
+
+        n = n -  2**rounded_log
+
+        exponent_list.append(rounded_log)
+
+    for exponent in exponent_list:
+        output += 10**exponent
+
+    return(output)
--- 001/tests.py
+++ 001/tests.py
@@ -1,7 +1,11 @@
 from code import check_for_symmetry
 from code import sum_of_first_n_numbers
 from code import convert_to_numbers, convert_to_letters
-
+from code import is_prime
+from code import get_intersection, get_union
+from code import count_characters
+from code import get_first_n_terms_nonrecursive, get_nth_term_recursive
+from code import convert_to_base_10, convert_to_base_2
 
 # check_for_symmetry tests
 if check_for_symmetry('racecar') is not True:
@@ -42,3 +46,76 @@ if convert_to_letters([1, 2, 3, 4, 5]) != "abcde":
 
 if convert_to_letters([0, 1, 2, 27]) != " abA":
     print('convert_to_letters failed on input "[0, 1, 2, 27]"')
+
+# is_prime tests
+if is_prime(10) is not False:
+    print('is_prime failed on input 10')
+
+if is_prime(11) is not True:
+    print('is_prime failed on input 11')
+    print(is_prime(11))
+
+# get_intersection tests
+if get_intersection([1, 2, 3, 4, 5, 1], [1, 2, 3, 4, 6, 7, 8]) != [1, 2, 3, 4]:
+    print('get_intersection failed on input "[1, 2, 3, 4, 5, 1], [1, 2, 3, 4, 6, 7, 8]"')
+    print(get_intersection([1, 2, 3, 4, 5, 1], [1, 2, 3, 4, 6, 7, 8]))
+
+if get_intersection([1, 1, 1, 1, 1], []) != []:
+    print('get_intersection failed on input "[1, 1, 1, 1, 1], []"')
+    print(get_intersection([1, 1, 1, 1, 1], []))
+
+# get_union tests
+
+if get_union([1, 2, 3, 4, 5, 5, 5, 5], [1, 5, 7, 8]) != [1, 2, 3, 4, 5, 7, 8]:
+    print('get_union is a failure')
+    print(get_union([1, 2, 3, 4, 5, 5, 5, 5], [1, 5, 7, 8]))
+
+if get_union(['a', 'b', 1, 2, 3], [1, 4, 5, 6, 'b']) != ['a', 'b', 1, 2, 3, 4, 5, 6]:
+    print('get_union is a failure')
+    print(get_union(['a', 'b', 1, 2, 3], [1, 4, 5, 6, 'b']))
+
+# count_characters tests
+
+test1 = dict({'a': 1, 'b': 1, 'c': 1})
+
+assert count_characters('abc') == test1
+
+test2 = dict({' ': 1, '!': 1, 'a': 2})
+
+assert count_characters('a a!') == test2
+
+# get_first_n_terms_nonrecursive tests
+
+nonrecursive_test1 = [5, 11, 29]
+
+assert get_first_n_terms_nonrecursive(3) == nonrecursive_test1
+
+nonrecursive_test2 = [5, 11, 29, 83]
+
+assert get_first_n_terms_nonrecursive(4) == nonrecursive_test2
+
+# get_nth_term_recursive tests
+
+get_nth_term_recursive_test1 = 83
+
+assert get_nth_term_recursive(4) == get_nth_term_recursive_test1
+
+get_nth_term_recursive_test2 = 245
+
+assert get_nth_term_recursive(5) == get_nth_term_recursive_test2
+
+# convert_to_base_10 tests
+
+convert_to_base_10_test1 = 7
+
+assert convert_to_base_10(111) == convert_to_base_10_test1
+
+convert_to_base_10_test2 = 11
+
+assert convert_to_base_10(1011) == convert_to_base_10_test2
+
+# convert_to_base_2 tests
+
+assert convert_to_base_2(10) == 1010
+
+assert convert_to_base_2(1001) == 1100100
