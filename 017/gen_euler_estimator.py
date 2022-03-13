a740e77f5289fcaf9986d004864eed9fcc1bd4ea
eliskol <75465281+eliskol@users.noreply.github.com>
2022-03-12 18:39:03 -0800

    rename dimension to function

--- 017/gen_euler_estimator.py
+++ 017/gen_euler_estimator.py
@@ -1,15 +1,15 @@
 class EulerEstimator:
     def __init__(self, derivatives):
         self.derivatives = derivatives
-        self.dimensions = [key for key in derivatives]
+        self.functions = [key for key in derivatives]
 
     def calc_derivative_at_point(self, point):
-        return {dimension: self.derivatives[dimension](point[0], point[1]) for dimension in self.dimensions}
+        return {function: self.derivatives[function](point[0], point[1]) for function in self.functions}
 
     def calc_estimated_points(self, point, step_size, num_steps):
         current_point = point
         point_array = [current_point]
         for i in range(num_steps):
-            current_point = (current_point[0] + step_size, {dimension: current_point[1][dimension] + step_size * self.calc_derivative_at_point(current_point)[dimension] for dimension in self.dimensions})
+            current_point = (current_point[0] + step_size, {function: current_point[1][function] + step_size * self.calc_derivative_at_point(current_point)[function] for function in self.functions})
             point_array.append(current_point)
         return point_array
