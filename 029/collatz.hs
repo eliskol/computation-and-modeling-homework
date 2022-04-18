91a341c3509e1d7cebf5efd9e9d4b6c617bc34ec
Elias <75465281+eliskol@users.noreply.github.com>
2022-04-18 23:40:48 +0000

    implemented firstChainLongerThan function

--- 029/collatz.hs
+++ 029/collatz.hs
@@ -3,4 +3,4 @@ chain n
     | even n = n: chain (n `div` 2)
     | odd n = n:chain (n * 3 + 1)
 
-firstChainLongerThan n = length
+firstChainLongerThan n = length (takeWhile (< n) [length(chainArr) | chainArr <- [chain i | i <- [1..]]]) + 1
